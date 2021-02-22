8dbdeeec9c3868099ba700f45252933269b0ed21
julianCLC <55823495+julianCLC@users.noreply.github.com>
2021-02-21 22:48:55 -0800

    Add files via upload

--- src/milestone1_1.4_1.5_main.py
+++ src/milestone1_1.4_1.5_main.py
@@ -8,53 +8,6 @@ print("program start")
 # 1.4 TRANSFORMATION
 # ====================================
 
-# ------------------
-# 1.4 functions
-# ------------------
-'''
-def combine_rows(grouped_df):
-
-    # Condense df into 1 row, combining the attributes
-    # Add all of rows except case fatality ratio
-    # Case fatality ratio = deaths / cases
-
-    # Preserve keys
-    province_state = grouped_df.iloc[0]["Province_State"]
-    country_region = grouped_df.iloc[0]["Country_Region"]
-    keys = {"Province_State": province_state, "Country_Region": country_region}
-
-    # Condense rows
-    temp_se = grouped_df[["Province_State",
-                          "Country_Region",
-                          "Confirmed",
-                          "Deaths",
-                          "Recovered",
-                          "Active",
-                          "Incidence_Rate"]].sum(min_count=1,
-                                                 numeric_only=True)
-
-    # Calculate case fatality
-    deaths = temp_se["Deaths"]
-    cases = temp_se["Confirmed"]
-    if cases == 0:
-        CF_ratio = 0
-    else:
-        CF_ratio = (deaths / cases) * 100
-  
-    # Build new row
-    item = {"Case-Fatality_Ratio": CF_ratio}
-    key_cols = pd.Series(data = keys)
-    new_col = pd.Series(data = item)
-    
-    new_row = key_cols.append(temp_se)
-    new_row = new_row.append(new_col)
-
-    return new_row
-'''
-# ------------------
-# 1.4 main
-# ------------------
-
 # Get location dataset
 filename = 'dataset/location.csv'
 loc_csv = open(filename, 'rt')
@@ -136,15 +89,8 @@ loc_csv = open(filename, 'rt')
 loc_df = pd.read_csv(filename)
 loc_csv.close()
 
-#print("before join:")
-#print(cases_df.iloc[0])
-
+# Join location and cases datasets
 joint_df = pd.merge(cases_df, loc_df, how="left")
-
-#print("===========================================================================")
-#print("after join:")
-#print(joint_df.iloc[0])
-
 
 # writing to CSV
 result_filename = 'dataset/cases_joined.csv'
