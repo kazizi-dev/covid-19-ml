41db6429d8147e57e0272f545dc11b4c2e3d199a
kazizi-dev <kouroshazizi2012@gmail.com>
2021-03-20 00:51:14 -0700

    build ada model

--- milestone-2/src/main.py
+++ milestone-2/src/main.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
 import os
+import pickle
 
 
 def encode_dataset(df):
@@ -21,14 +22,24 @@ def split_dataset(df):
     Split the dataset to 80:20
     """
     encode_dataset(df)
-    x_train, x_test, y_train, y_test = train_test_split(df['outcome'], 
-                                                        df.drop('outcome', axis=1), 
-                                                        test_size=0.2, 
-                                                        random_state=0)
 
-    # return two tuples
+    x = df.drop('outcome', axis=1)
+    y = df['outcome']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
     return x_train, y_train, x_test, y_test
 
+
+def build_ada_boost_model(x_train, y_train, x_test, y_test):
+    model = AdaBoostClassifier(n_estimators=20, learning_rate=0.8, random_state=0)
+    model = model.fit(x_train, y_train)
+    
+    path = './models/ada_boost_model.pkl'
+    pickle.dump(model, open(path, 'wb'))
+    model = pickle.load(open(path, 'rb'))
+    
+    print(f'Train Accuracy: {model.score(x_train, y_train)}')
+    print(f'Test Accuracy: {model.score(x_test, y_test)}')
 
 if __name__ == '__main__':
     print('--------------------------')
@@ -40,5 +51,7 @@ if __name__ == '__main__':
 
     x_train, y_train, x_test, y_test = split_dataset(df)
 
+
+    build_ada_boost_model(x_train, y_train, x_test, y_test)
 
     # df.to_csv('./data/test.csv', index=False)
