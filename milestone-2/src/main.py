31022a1a1a82131c15b70430263917c31e24d09a
julianCLC <55823495+julianCLC@users.noreply.github.com>
2021-03-21 22:14:31 -0700

    Added Random Forest classifier

--- milestone-2/src/main.py
+++ milestone-2/src/main.py
@@ -55,3 +55,84 @@ if __name__ == '__main__':
     build_ada_boost_model(x_train, y_train, x_test, y_test)
 
     # df.to_csv('./data/test.csv', index=False)
+    
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~ 2.2 RANDOM FORESTS ~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~ Get data ~~~~
+filename = 'data/cases_joined.csv'
+training = open(filename, 'rt')
+csv_reader = csv.reader(training, dialect = 'excel')
+training_df = pd.read_csv(filename)
+training.close()
+
+# ~~~~ Split data and labels ~~~~
+# dropped all location columns except for country
+# to minimize use of one hot encoding
+training_labels = training_df[['outcome']].values
+training_labels = training_labels.ravel()
+training_data = training_df.drop(['outcome',
+                                  'date_confirmation',
+                                  'longitude',
+                                  'latitude',
+                                  'province'], axis=1)
+
+# ~~~~ One hot encoding ~~~~
+one_hot_enc = OneHotEncoder()
+categorical_data = training_data[['sex', 'country']]
+
+binary_data = one_hot_enc.fit_transform(categorical_data).toarray()
+binary_labels = np.append(one_hot_enc.categories_[0], one_hot_enc.categories_[1])
+
+encoded_df = pd.DataFrame(binary_data, columns=binary_labels)
+
+# numerical data
+numerical_df = training_data.drop(['sex', 'country'], axis=1)
+
+# append converted data and numerical data
+numerical_df = numerical_df.join(encoded_df)
+
+# fill NaN values
+print("...filling NaN values")
+filled_encoded_df = numerical_df.fillna(value=0)
+
+# ~~~~ Classifier ~~~~
+print("...building model")
+rand_forest_clf = RandomForestClassifier()
+
+model = rand_forest_clf.fit(filled_encoded_df, training_labels)
+
+# save model
+print("...saving model")
+pkl_filename = "./models/rf_classifier.pkl"
+with open(pkl_filename, 'wb') as file:
+    pickle.dump(model, file)
+
+
+'''
+# writing to CSV
+filled_encoded_df = filled_encoded_df.join(training_df[['outcome']])
+print("...writing to csv")
+result_filename = 'data/cases_joined_converted.csv'
+#numerical_df.to_csv(result_filename, index=False)
+filled_encoded_df.to_csv(result_filename, index=False)
+'''
+
+filled_encoded_df = filled_encoded_df.join(training_df[['outcome']])
+
+# Load model
+print("...loading model")
+pkl_filename = "rf_classifier.pkl"
+with open(pkl_filename, 'rb') as file:
+    pickle_model = pickle.load(file)
+
+# Use model
+print("...using model on data")
+Xdata = filled_encoded_df.drop(['outcome'], axis=1)
+Ydata = filled_encoded_df[['outcome']]
+
+# print score
+score = pickle_model.score(Xdata, Ydata)
+print("Train score: {0:.2f} %".format(100 * score))
+Ypredict = pickle_model.predict(Xdata)
