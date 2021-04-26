Fixed new line in precision.txt

milestone-3/src/random_forest.py
@@ -6,19 +6,16 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score, GridSearchCV
-from sklearn.metrics import classification_report
-import os
-import matplotlib.pyplot as plt
-from sklearn import metrics as met
-from matplotlib import pyplot
+from sklearn.model_selection import  GridSearchCV
 
-from sklearn.metrics import make_scorer, accuracy_score, recall_score, f1_score, precision_score
+import os
+
+from sklearn.metrics import make_scorer, accuracy_score, recall_score, f1_score
 
 import warnings
 
-from helper import clean_age_data, impute_age_data, clean_sex_data
-from helper import clean_date, clean_cols, clean_num_cols, remove_unused_cols, handle_skewed_data
+from helper import impute_age_data, clean_sex_data
+from helper import clean_date, clean_num_cols, remove_unused_cols
 
 def split_dataset(df):
     """
@@ -37,8 +34,6 @@ def split_dataset(df):
     categorical_data = x_train[['sex']]   
     binary_data = enc.fit_transform(categorical_data).toarray()
     binary_labels = enc.categories_[0]
-    #binary_labels = np.append(enc.categories_[0], enc.categories_[1])
-    #binary_labels = np.append(binary_labels, enc.categories_[2])
 
     encoded_df = pd.DataFrame(binary_data, columns=binary_labels)
     x_train = x_train.drop(['sex', 'country', 'province'], axis=1)
@@ -51,16 +46,16 @@ def split_dataset(df):
 
 def get_grid_search_cv(x_train, y_train, model):
     params= {  
-        'n_estimators': [10, 100, 200, 400, 600, 800, 1000],
+        'n_estimators': [10, 100, 400, 800, 1000],
         'max_features': ['auto', 'sqrt', 'log2'],
         'max_depth' : [25],
         'criterion' :['entropy']
     }
 
     scoring = {
-        'f1_score_on_deceased' : make_scorer(f1_score, average='micro', labels=['deceased']),
+        'f1_score_on_deceased' : make_scorer(f1_score, labels=['deceased']),
         'overall_accuracy': make_scorer(accuracy_score),
-        'recall_on_deceased' : make_scorer(recall_score, average='micro', labels=['deceased']),
+        'recall_on_deceased' : make_scorer(recall_score, labels=['deceased']),
         'overall_recall': make_scorer(recall_score , average='weighted')
     }
 
@@ -131,13 +126,6 @@ def test_cases_test(csv_path):
     # setup path for main.py file
     os.chdir(os.getcwd())
 
-    #file_path = '../data/cases_test.csv'
-    #cases_df = pd.read_csv(file_path)
-    
-    # ========================================
-    # 1.5 JOINING DATASETS
-    # ========================================
-    
     # Get cases dataset
     filename = '../data/cases_test.csv'
     cases_csv = open(filename, 'rt')
@@ -151,8 +139,6 @@ def test_cases_test(csv_path):
     loc_csv.close()
     
     # Join location and cases datasets
-    #joint_df = pd.merge(cases_df, loc_df, how="left")
-    #joint_df = joint_df.fillna(0)
     cases_df = pd.merge(cases_df, loc_df, how="left")
 
     ## display missing values for cases_train file
@@ -161,16 +147,13 @@ def test_cases_test(csv_path):
 
     clean_sex_data(cases_df)
     impute_age_data(cases_df.age.tolist(), cases_df)
-    # cases_df = handle_skewed_data(cases_df)
     cases_df = remove_unused_cols(cases_df)
     clean_date(cases_df)
     clean_num_cols(cases_df, ['Confirmed', 'Deaths', 'Recovered', 'Active', 'Incidence_Rate', 'Case-Fatality_Ratio'])
-    #clean_cols(cases_df, ["country", "province"])
     cases_df = cases_df.drop(['country', 'province'], axis=1)
     
     # writing to CSV
     result_filename = '../data/cases_test_processed.csv'
-    #joint_df.to_csv(result_filename, index=False)
     cases_df.to_csv(result_filename, index=False)
 
     
@@ -181,57 +164,64 @@ def make_prediction(model, test_df):
     
     test_df['outcome'] = predictions
     
+    # csv with labels
     test_df.to_csv('../results/cases_test_predictions.csv', index=False)
-    print()
+    
+    # save to txt file
+    np.savetxt('../results/predictions.txt', predictions, fmt='%s')
+    
+    # fix newline 
+    # from SO: https://stackoverflow.com/questions/28492954/numpy-savetxt-stop-newline-on-final-line
+    with open('../results/predictions.txt', 'w') as fout:
+        NEWLINE_SIZE_IN_BYTES = 2 # 2 on Windows?
+        np.savetxt(fout, predictions, fmt='%s') # Use np.savetxt.
+        fout.seek(0, os.SEEK_END) # Go to the end of the file.
+        # Go backwards one byte from the end of the file.
+        fout.seek(fout.tell() - NEWLINE_SIZE_IN_BYTES, os.SEEK_SET)
+        fout.truncate() # Truncate the file to this point.
     
 def encode_predict():
-    # process cases_test
+    # Process cases_test
     data_path = '../data/cases_test_processed.csv' # name of processed test data
     test_cases_test(data_path)
     
-    # Load Model and Data
-    
+    # Load Model
     path = '../models/random_forest_model.pkl'
-    
     with open(path, 'rb') as f:
         model = pickle.load(f)
         
+    # Load Data
     df = pd.read_csv('../data/cases_test_processed.csv')
     
-    
-    # encoding joined cases_test data
-    
+    # Encoding joined cases_test data
     df['date_confirmation'] = LabelEncoder().fit_transform(df['date_confirmation'].astype(str))
-    #df['additional_information'] = LabelEncoder().fit_transform(df['additional_information'].astype(str))
-    
-    y_labels = df[['outcome']]
-    #y_labels = y_labels.ravel()
     
     enc = OneHotEncoder()
     x = df.drop(['outcome'], axis=1).copy()
-    #categorical_data = x[['sex', 'country', 'province']]
     categorical_data = x[['sex']]   
     binary_data = enc.fit_transform(categorical_data).toarray()
     binary_labels = enc.categories_[0]
-    #binary_labels = np.append(enc.categories_[0], enc.categories_[1])
-    #binary_labels = np.append(binary_labels, enc.categories_[2])
     
     encoded_df = pd.DataFrame(binary_data, columns=binary_labels)
     x = x.drop(['sex'], axis=1)
     x = x.join(encoded_df)
-    #x = x.join(y_labels)
-    #x = x.fillna(x.mean())
     
     x.to_csv('../data/cases_test_encoded.csv', index=False)
     
     make_prediction(model, x)
     
-
+def check_if_file_valid(filename):
+    assert filename.endswith('predictions.txt'), 'Incorrect filename'
+    f = open(filename).read()
+    l = f.split('\n')
+    assert len(l) == 46500, 'Incorrect number of items'
+    assert (len(set(l)) == 4), 'Wrong class labels'
+    return 'The predictions file is valid'
 
 # ============================================================================================
 
-
 start_random_forest()
 encode_predict()
+check_if_file_valid('../results/predictions.txt')
 
 print("~~~~~~~~~~end~~~~~~~~~~")